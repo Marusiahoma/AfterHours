6f8867380db54a22223d2837bee804e9f6f3c381
MacBook Air <macbookair@MacBook-Air-MacBook.local>
2020-05-14 03:16:29 +0300

    init

---
+++ mainApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'mainApp/homePage.html')
+
+
+def contact(request):
+    return render(request, 'mainApp/basic.html')
